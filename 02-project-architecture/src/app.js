d025e963e8c051b87ead28c97251a5b91a28900d
Aakriti Arya <aakritiarya2005@gmail.com>
2026-07-15 18:14:29 +0000

    feat: npm i cookie & cors & configuration

--- 02-project-architecture/src/app.js
+++ 02-project-architecture/src/app.js
@@ -1,5 +1,32 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
 
 const app = express();
+
+//configurating CORS -
+app.use(cors());
+//BUT with option
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+//config cookie 
+app.use(cookieParser);
+
+//config URL
+app.use(express.urlencoded({
+    extended: true,
+    limit: 16
+}))
+
+//config files/photos
+app.use(express.static("public"))
+
+//config JSON
+app.use(express.json({limit: 16}))
+
 
 export { app }
